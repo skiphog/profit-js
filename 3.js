c2425430a800f00a6bc8ccecb4c4200cc491670e
Skip <skiphog@mail.ru>
2017-09-27 15:26:40 +0300

    add 3 task

---
+++ 3.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function Calculator (number_1, number_2, operation) {
+  this.number_1 = parseInt(number_1);
+  this.number_2 = parseInt(number_2);
+  this.operation = operation;
+  this.result = null;
+
+  this.calc = function () {
+    if (null !== this.result) {
+      return this.result;
+    }
+
+    switch (this.operation) {
+      case '+' :
+        this.result = this.number_1 + this.number_2;
+        break;
+      case '-' :
+        this.result = this.number_1 - this.number_2;
+        break;
+    }
+
+    return this.result;
+  };
+}
+
+var calculator = new Calculator(2, 3, '+');
+
+console.log(calculator.calc()); // 5 (вычисление)
+console.log(calculator.calc()); // 5 (взято ранее вычисленное значение)
